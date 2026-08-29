0220eb3b274b536ac89e8d07165506bbaae92501
Jared Houghtaling <67749079+jshoughtaling@users.noreply.github.com>
2026-08-29 11:28:09 +0200

    Delete tests/local/test_signal.py
    Delete redundant file

--- tests/local/test_signal.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ["MSGBOX_SIGNAL_REST_URL"] = "http://127.0.0.1:8080"
-os.environ["MSGBOX_SIGNAL_NUMBER"] = "+32456890336"   # your linked number
-
-from messagebox.providers import SignalProvider
-p = SignalProvider()
-result = p.send_voice("+32456890336", "/Users/jaredwork/Documents/GitHub/button-box/tests/local/test.ogg", lock_wait=None)
-print(result)          # SendResult(ok=True, provider_message_id="...", ...)
-print(p.receive())     # after the other party replies
